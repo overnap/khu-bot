9048eb45e9d9da0cd51f5c8f0e2a69835bcadf17
Overnap <overnapworks@gmail.com>
2020-07-27 05:23:45 +0900

    init

---
+++ crawl.py
@@ -0,0 +1,197 @@
+# Let us process asynchronously
+import asyncio
+import functools
+# For some simple text processing
+import re
+# Import crawling libraries
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+# On this code, only the timeout exception is handled
+from selenium.common.exceptions import TimeoutException
+# For headless Firefox
+from selenium.webdriver.firefox.options import Options
+options = Options()
+options.add_argument("--headless")
+
+# Post data
+class Post:
+
+    def __init__(self, title, date, link):
+        self.title = title
+        self.date = date
+        self.link = link
+    
+    def __eq__(self, other):
+        return self.title == other.title and self.date == other.date
+
+async def khu_undergraduate_crawl():
+
+    data = []
+
+    try:
+        loop = asyncio.get_event_loop()
+
+        # Timeout after 30 seconds of no load
+        req = functools.partial(requests.get, "https://www.khu.ac.kr/kor/notice/list.do?category=UNDERGRADUATE&page=1", timeout=30)
+        web = await loop.run_in_executor(None, req)
+        soup = BeautifulSoup(web.content, "lxml")
+        raw_data = soup.find("table", {"class": "board01"}).find("tbody").find_all("td", {"class": "col02"})
+        for content in raw_data:
+            # Ignore the Seoul campus mark
+            if content.a.span.text == "서울":
+                continue
+            title = content.a.p.text.strip()
+            link = "https://www.khu.ac.kr/kor/notice/" + content.a.attrs["href"]
+            content = content.find_next_sibling("td", {"class": "col04"})
+            date = int(content.text.replace('-', ''))
+            data.append(Post(title, date, link))
+
+    except requests.exceptions.Timeout:
+        print("[Timeout] KHUS crawling timeout")
+    except Exception as error:
+        print("[ERROR] KHUS unknown error")
+        print("Error :", error)
+    finally:
+        return data
+
+async def sw_business_crawl():
+
+    data = []
+
+    try:
+        loop = asyncio.get_event_loop()
+
+        # Timeout after 30 seconds of no load
+        req = functools.partial(requests.get, "http://swedu.khu.ac.kr/board5/bbs/board.php?bo_table=06_01", timeout=30)
+        web = await loop.run_in_executor(None, req)
+        soup = BeautifulSoup(web.content, "lxml")
+        raw_data = soup.find_all("td", {"class": "td_num2"})
+        regex = re.compile('종료')
+
+        for content in raw_data:
+            # TODO: Notice mark identification, not used but can be used
+            # notice = True if content.text.strip() == "공지" else False
+            content = content.find_next_sibling("td", {"class": "td_subject"})
+            # skip if there is an [END] mark
+            if regex.search(content.a.text.strip()) != None:
+                continue
+            title = content.a.text.strip()
+            link = content.a.attrs["href"]
+            content = content.find_next_sibling("td", {"class": "td_datetime"})
+            date = int(content.text.replace('-', ''))
+            data.append(Post(title, date, link))
+
+    except requests.exceptions.Timeout:
+        print("[Timeout] SWB crawling timeout")
+    except Exception as error:
+        print("[ERROR] SWB unknown error")
+        print("Error :", error)
+    finally:
+        return data
+
+async def sw_college_crawl():
+
+    data = []
+
+    try:
+        loop = asyncio.get_event_loop()
+
+        # Timeout after 30 seconds of no load
+        req = functools.partial(requests.get, "http://software.khu.ac.kr/board5/bbs/board.php?bo_table=05_01", timeout=30)
+        web = await loop.run_in_executor(None, req)
+        soup = BeautifulSoup(web.content, "lxml")
+        raw_data = soup.find_all("td", {"class": "td_subject"})
+        
+        for content in raw_data:
+            # TODO: Important mark identification, not used but can be used
+            # important = True if content.text.strip() == "중요" else False
+            title = content.a.text.strip()
+            link = content.a.attrs["href"]
+            content = content.find_next_sibling("td", {"class": "td_datetime"})
+            date = int(content.text.replace('-', ''))
+            data.append(Post(title, date, link))
+
+    except requests.exceptions.Timeout:
+        print("[Timeout] SWC crawling timeout")
+    except Exception as error:
+        print("[ERROR] SWC unknown error")
+        print("ERROR :", error)
+    finally:
+        return data
+
+async def j_dormitory_crawl():
+
+    data = []
+
+    try:
+        loop = asyncio.get_event_loop()
+        drv = functools.partial(webdriver.Firefox, options=options)
+        driver = await loop.run_in_executor(None, drv)
+    except Exception as error:
+        print("[ERROR] J_DORMIT selenium firefox driver error")
+        print("ERROR :", error)
+        return data
+
+    try:
+        # Timeout after 60 seconds of no load
+        driver.set_page_load_timeout(60)
+        await loop.run_in_executor(None, driver.get, "https://dorm2.khu.ac.kr/dorm2/00/0000.kmc")
+
+        # Waiting for web load to avoid errors
+        await asyncio.sleep(2)
+
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        raw_data = soup.find("ul", {"id": "board_notice"}).find_all("li")
+
+        for content in raw_data:
+            title = content.a.text.strip()
+            link = "https://dorm2.khu.ac.kr/dorm2/bbs/getBbsWriteView.kmc?bbs_locgbn=K1&bbs_id=notice&seq="
+            link += content.a.attrs["onclick"][8:13]
+            date = int(content.span.text.replace('.', ''))
+            post = Post(title, date, link)
+            data.append(post)
+
+    except TimeoutException:
+        print("[Timeout] J_DORMIT crawling timeout")
+    except Exception as error:
+        print("[ERROR] J_DORMIT unknown error")
+        print("ERROR :", error)
+    finally:
+        driver.quit()
+        return data
+
+async def j_meal_crawl():
+
+    data = []
+    
+    try:
+        loop = asyncio.get_event_loop()
+        drv = functools.partial(webdriver.Firefox, options=options)
+        driver = await loop.run_in_executor(None, drv)
+    except Exception as error:
+        print("[ERROR] J_MEAL selenium firefox driver error")
+        print("ERROR :", error)
+    
+    try:
+        # Timeout after 60 seconds of no load
+        driver.set_page_load_timeout(60)
+        await loop.run_in_executor(None, driver.get, "https://dorm2.khu.ac.kr/dorm2/40/4050.kmc")
+
+        # Waiting for web load to avoid errors
+        await asyncio.sleep(2)
+
+        soup = BeautifulSoup(driver.page_source, "lxml")
+        raw_data = soup.find("li", {"style": "display: list-item;"}).table.find_all("td", {"class": "bb te_left pl15"})
+
+        for content in raw_data:
+            data.append(content.text)
+
+    except TimeoutException:
+        print("[Timeout] J_MEAL crawling timeout")
+    except Exception as error:
+        print("[ERROR] J_MEAL unknown error")
+        print("ERROR :", error)
+    finally:
+        driver.quit()
+        return data
---
+++ main.py
@@ -0,0 +1,206 @@
+# Let us process asynchronously
+import asyncio
+# Import discord API
+import discord
+from discord.ext.commands import Bot
+# For convenient time handling
+from datetime import datetime
+from pytz import timezone
+# For serializing config
+import json
+# Import custom crawling module
+import crawl
+
+with open(".\config.json") as file:
+    config = json.load(file)
+
+is_running = False
+bot = Bot(command_prefix=config["PREFIX"])
+
+channels = []
+khuu_data = []
+j_data = []
+j_meal = []
+swb_data = []
+swc_data = []
+check_meal = [False, False, False]
+
+async def khu_undergraduate_alert():
+    crawled = await crawl.khu_undergraduate_crawl()
+    for post in crawled:
+        exist = any(x == post for x in khuu_data)
+        if not exist:
+            print("[Main] New Post :", post.title)
+            embed = discord.Embed(title=post.title, description=post.link, color=0xEB422E)
+            embed.set_author(name="KHU 학사", url="https://www.khu.ac.kr/kor/notice/list.do?page=1&category=UNDERGRADUATE", icon_url="http://www.google.com/s2/favicons?domain=https://www.khu.ac.kr/kor/main/index.do")
+            for channel in channels:
+                await channel.send(embed=embed)
+            khuu_data.append(post)
+            # Delete old posts for memory management
+            if len(swb_data) > 20:
+                khuu_data.pop(0)
+
+async def sw_business_alert():
+    crawled = await crawl.sw_business_crawl()
+    for post in crawled:
+        exist = any(x == post for x in swb_data)
+        if not exist:
+            print("[Main] New Post :", post.title)
+            embed = discord.Embed(title=post.title, description=post.link, color=0xE060A4)
+            embed.set_author(name="SW 사업단", url="http://swedu.khu.ac.kr/html_2018/", icon_url="http://www.google.com/s2/favicons?domain=https://www.khu.ac.kr/kor/main/index.do")
+            for channel in channels:
+                await channel.send(embed=embed)
+            swb_data.append(post)
+            # Delete old posts for memory management
+            if len(swb_data) > 20:
+                swb_data.pop(0)
+
+async def sw_college_alert():
+    crawled = await crawl.sw_college_crawl()
+    for post in crawled:
+        exist = any(x == post for x in swc_data)
+        if not exist:
+            print("[Main] New Post :", post.title)
+            embed = discord.Embed(title=post.title, description=post.link, color=0xF5D356)
+            embed.set_author(name="SW융합대학", url="http://software.khu.ac.kr/html_2018/", icon_url="http://www.google.com/s2/favicons?domain=https://www.khu.ac.kr/kor/main/index.do")
+            for channel in channels:
+                await channel.send(embed=embed)
+            swc_data.append(post)
+            # Delete old posts for memory management
+            if len(swb_data) > 20:
+                swc_data.pop(0)
+
+async def j_dormitory_alert():
+    crawled = await crawl.j_dormitory_crawl()
+    for post in crawled:
+        exist = any(x == post for x in j_data)
+        if not exist:
+            print("[Main] New Post :", post.title)
+            embed = discord.Embed(title=post.title, description=post.link, color=0x568DF5)
+            embed.set_author(name="제2기숙사", url="https://dorm2.khu.ac.kr/dorm2/", icon_url="http://www.google.com/s2/favicons?domain=https://www.khu.ac.kr/kor/main/index.do")
+            for channel in channels:
+                await channel.send(embed=embed)
+            j_data.append(post)
+            # Delete old posts for memory management
+            if len(swb_data) > 15:
+                j_data.pop(0)
+
+async def j_meal_alert(t:datetime):
+    # TODO: It would be good to manage diet data for each channel
+    # TODO: Prettify spaghetti code
+    global j_meal
+    global check_meal
+    # Try crawling if it is a new day or do not have data
+    if (t.hour > 4 and t.hour < 6 and check_meal.count(True) == 3) or len(j_meal) == 0:
+        check_meal = [False, False, False]
+        try:
+            j_meal = await crawl.j_meal_crawl()
+            print("[Main] Got meal data")
+        except:
+            j_meal = []
+            print("[ERROR] Cannot get meal data")
+        print("[Main] Today's date :", t.date(), ", day of week :", t.weekday())
+    # Check all if meal time have passed or do not have data
+    if t.hour > 6 and len(j_meal) == 0:
+        print("[Main] No meal data for today!")
+        check_meal = [True, True, True]
+    elif t.hour > 19:
+        check_meal = [True, True, True]
+    # Dinner
+    elif ((t.hour == 17 and t.minute > 5) or t.hour > 17) and check_meal[2] == False:
+        check_meal = [True, True, True]
+        embed = discord.Embed(title="제2기숙사", description="석식 메뉴", color=0x59DE6D)
+        embed.set_author(name="오늘의 학식", url="https://dorm2.khu.ac.kr/dorm2/40/4050.kmc", icon_url="http://www.google.com/s2/favicons?domain=https://www.khu.ac.kr/kor/main/index.do")
+        if t.weekday() == 5 or t.weekday() == 6:
+            # Korean food only on weekends
+            embed.add_field(name="한식", value="없음" if j_meal[2] == '' else j_meal[2], inline=False)
+        else:
+            embed.add_field(name="한식", value="없음" if j_meal[5] == '' else j_meal[5], inline=False)
+            embed.add_field(name="일품", value="없음" if j_meal[6] == '' else j_meal[6], inline=False)
+        for channel in channels:
+            await channel.send(embed=embed)
+    # Lunch
+    elif ((t.hour == 10 and t.minute > 40) or t.hour > 10) and check_meal[1] == False:
+        check_meal = [True, True, False]
+        embed = discord.Embed(title="제2기숙사", description="중식 메뉴", color=0x59DE6D)
+        embed.set_author(name="오늘의 학식", url="https://dorm2.khu.ac.kr/dorm2/40/4050.kmc", icon_url="http://www.google.com/s2/favicons?domain=https://www.khu.ac.kr/kor/main/index.do")
+        if t.weekday() == 5 or t.weekday() == 6:
+            # Korean food only on weekends
+            embed.add_field(name="한식", value="없음" if j_meal[1] == '' else j_meal[1], inline=False)
+        else:
+            # Lunch has first class 1 and first class 2
+            embed.add_field(name="한식", value="없음" if j_meal[2] == '' else j_meal[2], inline=False)
+            embed.add_field(name="일품1", value="없음" if j_meal[3] == '' else j_meal[3], inline=False)
+            embed.add_field(name="일품2", value="없음" if j_meal[4] == '' else j_meal[4], inline=False)
+        for channel in channels:
+            await channel.send(embed=embed)
+    # Breakfast
+    elif ((t.hour == 7 and t.minute > 40) or t.hour > 7) and check_meal[0] == False:
+        check_meal = [True, False, False]
+        embed = discord.Embed(title="제2기숙사", description="조식 메뉴", color=0x59DE6D)
+        embed.set_author(name="오늘의 학식", url="https://dorm2.khu.ac.kr/dorm2/40/4050.kmc", icon_url="http://www.google.com/s2/favicons?domain=https://www.khu.ac.kr/kor/main/index.do")
+        if t.weekday() == 5 or t.weekday() == 6:
+            # Korean food only on weekends
+            embed.add_field(name="한식", value="없음" if j_meal[0] == '' else j_meal[0], inline=False)
+        else:
+            embed.add_field(name="한식", value="없음" if j_meal[0] == '' else j_meal[0], inline=False)
+            embed.add_field(name="일품", value="없음" if j_meal[1] == '' else j_meal[1], inline=False)
+        for channel in channels:
+            await channel.send(embed=embed)
+            
+async def main_coroutine():
+    # System main coroutine
+    
+    # To prevent multiple execution
+    global is_running
+    is_running = True
+
+    # Crawl and alarm
+    while True:
+        t = datetime.now(timezone("Asia/Seoul"))
+        # Record start time
+        print("[Main] Coroutine started : " + t.strftime("%Y/%m/%d %H:%M:%S"))
+
+        # For debugging, only work with more than one user
+        if len(channels) != 0:
+            futures = []
+            futures.append(asyncio.ensure_future(khu_undergraduate_alert()))
+            futures.append(asyncio.ensure_future(j_dormitory_alert()))
+            futures.append(asyncio.ensure_future(j_meal_alert(t)))
+            futures.append(asyncio.ensure_future(sw_college_alert()))
+            futures.append(asyncio.ensure_future(sw_business_alert()))
+            await asyncio.gather(*futures)
+
+        # Record completion time
+        print("[Main] Coroutine completed : " + datetime.now(timezone("Asia/Seoul")).strftime("%Y/%m/%d %H:%M:%S"))
+        # Repeat with delay
+        await asyncio.sleep(config["DELAY"])
+
+@bot.event
+async def on_ready():
+    print("[BOT] The bot is ready!")
+
+    # Run only once
+    global is_running
+    if is_running == False:
+        await main_coroutine()
+
+@bot.command()
+async def start(ctx):
+    if channels.count(ctx.channel) == 0:
+        channels.append(ctx.channel)
+        print("[BOT] The alert started :", ctx.channel)
+        await ctx.send("알리미가 시작되었습니다.")
+    else:
+        await ctx.send("이미 알리미가 실행 중입니다.")
+
+@bot.command()
+async def stop(ctx):
+    if channels.count(ctx.channel) != 0:
+        channels.remove(ctx.channel)
+        print("[BOT] The alert stopped :", ctx.channel)
+        await ctx.send("알리미가 정지되었습니다.")
+    else:
+        await ctx.send("알리미가 실행 중이지 않습니다.")
+
+bot.run(config["TOKEN"])
